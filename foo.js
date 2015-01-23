8575631d26595a12b68ff062b996e41b7f43508d
vlas86 <vlas86@gmail.com>
2015-01-23 12:03:34 +0300

    Update foo.js

--- foo.js
+++ foo.js
@@ -1,13 +1,8 @@
-<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-<script>
-	
-		$(function(){
-			var buttonClass = $('button.btn').attr('class');
-			$('li a').on('click', function(){
-				var liClass = $(this).attr('class');
-			$('div.btn-group button').removeAttr('class');
-			$('div.btn-group button').addClass(buttonClass+' '+liClass);
-			})
-		}());
-
-</script>
+(function($){
+            var buttonClass = $('button.btn').attr('class');
+            $('li a').on('click', function(){
+                var liClass = $(this).attr('class');
+            $('div.btn-group button').removeAttr('class');
+            $('div.btn-group button').addClass(buttonClass+' '+liClass);
+            })
+        })(jQuery);
