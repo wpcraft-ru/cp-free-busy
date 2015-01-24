1a38ff6cfb639dcc0136426b38b158879118998d
vlas86 <vlas86@gmail.com>
2015-01-24 13:17:58 +0300

    Delete foo.js

--- foo.js
+++
@@ -1,8 +0,0 @@
-(function($){
-            var buttonClass = $('button.btn').attr('class');
-            $('li a').on('click', function(){
-                var liClass = $(this).attr('class');
-            $('div.btn-group button').removeAttr('class');
-            $('div.btn-group button').addClass(buttonClass+' '+liClass);
-            })
-        })(jQuery);
