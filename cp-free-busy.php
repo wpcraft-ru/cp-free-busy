a16f963b4e8a2d1f8f745ebc31b453dc24e0ac42
vlas86 <vlas86@gmail.com>
2015-01-28 02:43:05 +0300

    Update cp-free-busy.php

--- cp-free-busy.php
+++ cp-free-busy.php
@@ -5,8 +5,8 @@ Version: 1.0.0
 Author: Vlad
 */
  
-function show_busy_button(){
- $data='<div class="btn-group ">
+function show_busy_button(){?>
+	<div class="btn-group ">
 		<button class="btn btn-default btn-sm dropdown-toggle " type="button" data-toggle="dropdown" >
 			Small button <span class="caret"></span>
 		
@@ -18,34 +18,32 @@ function show_busy_button(){
         <li ><a class="btn btn-success">Green</a></li>
        
       </ul>
-    </div>';
-return $data;
- 
-}
+    </div>
+<?php }  
 add_shortcode('cp-free-busy', 'show_busy_button');
-function button_color_script(){
+
+function btn_color_script(){
 	$script = "
 		<script type='text/javascript'>
-			jQuery(document).ready(function($){
+			jQuery(document).ready(function(jQuery){
 				var buttonClass = jQuery('button.btn').attr('class');
-				jQuery('li a').on('click', function(){
+				$('li a').on('click', function(){
 					var liClass = jQuery(this).attr('class');
 				jQuery('div.btn-group button').removeAttr('class');
 				jQuery('div.btn-group button').addClass(buttonClass+' '+liClass);
-				})
+				});
 			});
 		</script>	
 		";
 	return $script;
 	
 }
-add_action('wp_enqueue_scripts', 'button_color_script');
-
+add_action('wp_enqueue_scripts', 'btn_color_script');
 function my_scripts_method() {
-    
+    //wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
     wp_enqueue_script( 'jquery' );
 }    
  
 add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
-
 ?>
